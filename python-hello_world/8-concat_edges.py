3404d4397102c83a9b72ee4268b436e764f3980f
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-16 18:18:29 +0000

    ecvv

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+print(str[39:67] + " " + str[107:113] + " " + str[:6])
