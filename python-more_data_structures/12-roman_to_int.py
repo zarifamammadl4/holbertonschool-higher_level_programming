ab57cd349c8142ee7d0594a36eaafa9c9c0d4690
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-22 17:15:29 +0000

    dvvd

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
-        return 
+        return 0
 
     roman_map = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000}
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
 
     total = 0
     prev_value = 0
