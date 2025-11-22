073cb0668076effe775179b96c029d12f6425d95
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-22 16:10:59 +0000

    dvvd

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Ensure each tuple has at least 2 elements
+    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
+
+    return (a1 + b1, a2 + b2)
