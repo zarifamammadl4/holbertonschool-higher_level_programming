9b616a3d698c62ca976d428ee8652c62a5d94635
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-22 17:06:59 +0000

    dvvd

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
-        return 0
+def roman_to_int(num):
+    if not isinstance(num, int) or num < 1 or num > 3999:
+        return None
 
-    roman_map = {
-        I: 1, V: 5, X: 10,
-        L: 50, C: 100,
-        D: 500, M: 1000
-    }
-
-    total = 0
-    prev_value = 0
-
-    for char in reversed(roman_string):
-        value = roman_map.get(char, 0)
-        if value < prev_value:
-            total -= value
-        else:
-            total += value
-        prev_value = value
-
-    return total
+    val = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    ]
+    syms = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    ]
+    roman_num = ""
+    i = 0
+    while num > 0:
+        for _ in range(num // val[i]):
+            roman_num += syms[i]
+            num -= val[i]
+        i += 1
+    return roman_num
