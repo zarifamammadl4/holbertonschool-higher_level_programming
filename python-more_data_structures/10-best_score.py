b3226dc9a407a6e389845158d42c47c5532f397f
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-22 17:02:50 +0000

    dvvd

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,10 +2,8 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    best_key = None
-    best_value = float(-inf)
-    for key, value in a_dictionary.items():
-        if value > best_value:
-            best_value = value
+    best_key = list(a_dictionary.keys())[0]
+    for key in a_dictionary:
+        if a_dictionary[key] > a_dictionary[best_key]:
             best_key = key
     return best_key
