678cf6e37a3975d41e3eb748d9d0688b85ed499a
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-23 20:19:44 +0000

    fdf

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
         return 0
 
     roman = {
-       "I": 1, "V": 5, "X": 10, "L": 50,
+        "I": 1, "V": 5, "X": 10, "L": 50,
         "C": 100, "D": 500, "M": 1000
     }
 
@@ -15,20 +15,15 @@ def roman_to_int(roman_string):
     length = len(roman_string)
 
     while i < length:
-        # current value
         s1 = roman.get(roman_string[i], 0)
 
-        # look ahead
         if i + 1 < length:
             s2 = roman.get(roman_string[i + 1], 0)
-
-            # subtraction case
             if s1 < s2:
                 total += (s2 - s1)
                 i += 2
                 continue
 
-        # normal add
         total += s1
         i += 1
 
