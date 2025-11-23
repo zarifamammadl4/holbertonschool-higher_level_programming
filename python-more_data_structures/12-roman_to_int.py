03bdadd50de72d6e7c588d1957d794f34f03df3c
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-23 20:18:11 +0000

    fdf

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,8 +6,8 @@ def roman_to_int(roman_string):
         return 0
 
     roman = {
-        I: 1, V: 5, X: 10, L: 50,
-        C: 100, D: 500, M: 1000
+       "I": 1, "V": 5, "X": 10, "L": 50,
+        "C": 100, "D": 500, "M": 1000
     }
 
     total = 0
