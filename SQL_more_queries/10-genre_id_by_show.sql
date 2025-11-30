39d118b26d4a1add26874fc104b7869315a51e22
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 17:28:50 +0000

    sv

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- 10-genre_id_by_show.sql
+-- List all shows that have at least one genre linked
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
