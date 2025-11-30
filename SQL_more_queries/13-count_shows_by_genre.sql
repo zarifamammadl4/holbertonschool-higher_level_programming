ac1365ccbf92a701d06f01183c7deecaa9a8b9a7
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 17:32:01 +0000

    sv

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- 13-count_shows_by_genre.sql
+-- List all genres and the number of shows linked to each genre.
+-- Exclude genres with no shows and order by the number of shows in descending order.
+
+SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM genres
+LEFT JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.name
+HAVING COUNT(tv_show_genres.show_id) > 0
+ORDER BY number_of_shows DESC;
