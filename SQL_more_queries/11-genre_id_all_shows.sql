c3295d30e351ee60de480f2b7a5efd55c930dfbd
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 17:29:43 +0000

    sv

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- 11-genre_id_all_shows.sql
+-- List all shows and their genre_id (NULL if no genre)
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
