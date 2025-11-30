cbd7c777ef989efbdd884bbef79be431aaa0808b
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 12:19:44 +0000

    vfd

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,6 +4,7 @@ This module defines the lookup function, which returns the list of
 available attributes and methods of an object.
 """
 
+
 def lookup(obj):
     """Return the list of available attributes and methods of an object."""
     return dir(obj)
