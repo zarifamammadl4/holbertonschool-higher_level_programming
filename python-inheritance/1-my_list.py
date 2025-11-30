1d3dadec56171e4136e842637db7bdb9652e9f72
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 12:29:04 +0000

    vfd

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines the MyList class, a subclass of list with
+an additional method to print the list sorted.
+"""
+
+
+class MyList(list):
+    """A custom list class that can print itself sorted."""
+
+    def print_sorted(self):
+        """Print the list in ascending sorted order."""
+        print(sorted(self))
