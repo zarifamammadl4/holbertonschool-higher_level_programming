e65cb19511b182f90ecc07c7b8035475b1cf9337
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 18:55:16 +0000

    fvs

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,10 @@
--- Read user
-Grants for user_0d_2@localhost
-GRANT USAGE ON *.* TO `user_0d_2`@`localhost`
-GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`
+-- Create database (no fail)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user (fallback for older MySQL/MariaDB)
+CREATE USER user_0d_2@localhost IDENTIFIED BY user_0d_2_pwd;
+
+-- Give SELECT permission
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+
+FLUSH PRIVILEGES;
