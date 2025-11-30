a41f0d41cd7b12503608498aeb22ef348f54148d
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 14:05:30 +0000

    vfd

---
+++ python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a Student class"""
+
+class Student:
+    """Represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns a dictionary representation of a Student instance"""
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+        }
