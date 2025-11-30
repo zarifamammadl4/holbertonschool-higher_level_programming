b75ef939846fdf72636941860e7f3e64274d84cb
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 23:00:45 +0300

    vd

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
--- This script lists all records with non-empty names
+-- List all records with a valid name (not NULL, not empty)
 SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL AND name != 
