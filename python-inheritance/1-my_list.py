1b5612ae2d43a81fd69d4cbc18e3bcda41dc7b4d
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 12:38:14 +0000

    vfd

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,7 +9,5 @@ class MyList(list):
     """A custom list class that can print itself sorted."""
 
     def print_sorted(self):
-        """Print the list in ascending sorted order and return it."""
-        sorted_list = sorted(self)
-        print(sorted_list)
-        return sorted_list
+        """Print the list in ascending sorted order."""
+        print(sorted(self))
