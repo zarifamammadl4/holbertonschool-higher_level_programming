8ef311173942e475f7784fed65c744522f70f091
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 13:34:05 +0000

    vfd

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines a function to write a string to a UTF-8 text file.
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a UTF-8 text file and returns the number of characters written."""
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
