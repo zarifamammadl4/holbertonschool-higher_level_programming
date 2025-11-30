ac18100092e4f6fe7112302f751b43c7f96b54ba
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 18:50:10 +0000

    fvs

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,4 @@
--- Create user user_0d_1 only if it does not already exist
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
-
--- Grant all privileges on server
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
-
--- Apply privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
