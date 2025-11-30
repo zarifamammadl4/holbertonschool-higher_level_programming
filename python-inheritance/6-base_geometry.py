9ff822fcbcf7f4c7cff338541068d5421f6ba2d1
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 12:48:08 +0000

    vfd

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines the BaseGeometry class with an unimplemented area method.
+"""
+
+
+class BaseGeometry:
+    """Base class for geometry operations."""
+
+    def area(self):
+        """Raises an exception indicating the area method is not implemented."""
+        raise Exception("area() is not implemented")
