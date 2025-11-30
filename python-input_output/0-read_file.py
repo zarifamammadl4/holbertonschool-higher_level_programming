e3da1854fb503225684d1a2cc03620037b96bb24
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 13:32:05 +0000

    vfd

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""
+This module defines a function to read a UTF-8 text file and print its content.
+"""
+
 def read_file(filename=""):
+    """Reads a UTF-8 text file and prints its content to stdout."""
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
