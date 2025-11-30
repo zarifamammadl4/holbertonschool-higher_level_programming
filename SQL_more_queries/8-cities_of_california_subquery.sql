2a49ac90703adeda4961390273e1aced8636994b
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 16:15:30 +0000

    sv

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,11 @@
+-- 8-cities_of_california_subquery.sql
+-- List all cities of California without using JOIN
+
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = California
+)
+ORDER BY id ASC;
