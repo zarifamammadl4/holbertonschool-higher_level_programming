4b5df0e7e8e1b294c92a59dc210cfb7df41727e9
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 14:09:32 +0000

    vfd

---
+++ python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Defines a Student class"""
+
+
+class Student:
+    """Represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns a dictionary representation of a Student instance.
+        If attrs is a list of strings, only attributes in this list are included.
+        """
+        if isinstance(attrs, list) and all(
+            isinstance(a, str) for a in attrs
+        ):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+        }
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance.
+        json: dictionary with attribute names as keys
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
