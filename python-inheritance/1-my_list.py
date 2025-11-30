ba40eb6d6a048dbf4dd457c5890a6ef5b60b3223
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 18:06:05 +0000

    vd

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Module that defines a custom list class MyList."""
-
+"""
+Module that defines a custom list class MyList.
+"""
 
 class MyList(list):
     """A list that can print itself sorted."""
 
     def print_sorted(self):
         """Print the list in ascending sorted order (without modifying it)."""
-        print(sorted(self))
+        print(sorted(self))  # Works fine for an empty list as well
