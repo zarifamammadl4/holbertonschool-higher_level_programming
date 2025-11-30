92ece038a6fb0d243b6e807dfb603e2dd6fa2c91
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 14:17:28 +0000

    vfd

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Basic serialization and deserialization module"""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Serialize a Python dictionary to a JSON file.
+
+    Args:
+        data (dict): Python dictionary to serialize
+        filename (str): Name of the file to save the JSON data
+    """
+    with open(filename, w, encoding=utf-8) as f:
+        json.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    """
+    Load a JSON file and deserialize it to a Python dictionary.
+
+    Args:
+        filename (str): Name of the JSON file to read
+
+    Returns:
+        dict: Python dictionary representing the JSON data
+    """
+    with open(filename, r, encoding=utf-8) as f:
+        return json.load(f)
