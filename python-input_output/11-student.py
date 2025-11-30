e889bd0978c767f858e0ef942d7593da6bcb0afa
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-30 22:56:57 +0300

    vd

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Defines a Student class"""
+"""Defines a Student class."""
 
 
 class Student:
-    """Represents a student"""
+    """Represents a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student instance"""
+        """Initialize a Student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -14,11 +14,10 @@ class Student:
     def to_json(self, attrs=None):
         """
         Returns a dictionary representation of a Student instance.
-        If attrs is a list of strings, only attributes in this list are included.
+        If attrs is a list of strings, only those attributes are included.
         """
-        if isinstance(attrs, list) and all(
-            isinstance(a, str) for a in attrs
-        ):
+        if isinstance(attrs, list) and all(isinstance(a, str)
+                                           for a in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
 
         return {
@@ -29,8 +28,7 @@ class Student:
 
     def reload_from_json(self, json):
         """
-        Replaces all attributes of the Student instance.
-        json: dictionary with attribute names as keys
+        Replaces all attributes of the Student instance using json dict.
         """
         for key, value in json.items():
             setattr(self, key, value)
