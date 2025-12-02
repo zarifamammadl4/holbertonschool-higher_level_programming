03e3bb4ed7f463c6bca775b753f236d3776d0f92
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-12-02 16:19:17 +0000

    S

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- Create user user_0d_2 with password
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY user_0d_2_pwd;
+CREATE USER IF NOT EXISTS 'user_0d_2@localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant SELECT privilege on hbtn_0d_2 database
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2@localhost';
 
 FLUSH PRIVILEGES;
