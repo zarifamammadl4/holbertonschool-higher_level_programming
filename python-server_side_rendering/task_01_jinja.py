f4c667bee8c613b614183751b09f989b91b35bd7
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-12-02 14:39:23 +0000

    sfc

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -2,17 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route(/)
+@app.route('/')
 def home():
-    return render_template(index.html)
+    return render_template('index.html')
 
-@app.route(/about)
+@app.route('/about')
 def about():
-    return render_template(about.html)
+    return render_template('about.html')
 
-@app.route(/contact)
+@app.route('/contact')
 def contact():
-    return render_template(contact.html)
+    return render_template('contact.html')
 
-if __name__ == __main__:
+if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
