df0229045836abab3523139943806dad6328e9cc
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-12-02 16:20:50 +0000

    S

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,14 @@
--- Create user user_0d_2 with password
-CREATE USER IF NOT EXISTS 'user_0d_2@localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- 2-create_read_user.sql
 
--- Grant SELECT privilege on hbtn_0d_2 database
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2@localhost';
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
+-- Create the user if it does not exist, with password
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege only on hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Apply privileges
 FLUSH PRIVILEGES;
+
