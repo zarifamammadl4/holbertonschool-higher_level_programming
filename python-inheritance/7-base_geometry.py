729600b039ef9725d40fc14d57301a4bc471b33e
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-12-02 15:39:15 +0000

    Bf

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Module that defines the BaseGeometry class with validation."""
+"""
+Module that defines the BaseGeometry class with validation.
+"""
 
 
 class BaseGeometry:
@@ -13,15 +15,14 @@ class BaseGeometry:
         """Validate that value is an integer greater than 0.
 
         Args:
-            name (str): name of the parameter (always a string).
-            value: value to validate.
+            name (str): Name of the parameter.
+            value: Value to validate.
 
         Raises:
-            TypeError: if value is not an integer.
-            ValueError: if value <= 0.
+            TypeError: If value is not an integer.
+            ValueError: If value <= 0.
         """
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-
