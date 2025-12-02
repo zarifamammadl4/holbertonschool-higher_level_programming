8e42d37c6fdd343e428909481727b4afda176385
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-12-02 16:16:11 +0000

    S

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- List all records with a valid name (not NULL, not empty)
 SELECT score, name
 FROM second_table
-WHERE name IS NOT NULL AND name != 
+WHERE name IS NOT NULL AND name != ''
 ORDER BY score DESC;
